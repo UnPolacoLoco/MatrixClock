0ca962de2ddf4db0b327548c0fae099e24d0f533
UnPolacoLoco <31392246+UnPolacoLoco@users.noreply.github.com>
2018-02-22 21:01:17 +0100

    Got the real time clock module hooked up
    mode 0 (24hr clock) and mode 1 (date) are working

--- MatrixClockFinal.ino
+++ MatrixClockFinal.ino
@@ -1,5 +1,7 @@
 
 
+
+
 #define MATRIX_PIN 5
 #define SCL A5
 #define SDA A4
@@ -8,6 +10,10 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 #include <DS3231.h>
+#include <Arduino.h>
+#include <EEPROM.h>
+
+
 
 
 
@@ -29,7 +35,10 @@ const int GREEN = matrix.Color(0, 255, 0);
 const int BLUE = matrix.Color(0, 0, 255);
 
 byte mode = 0;
+byte setAddress = 0x02; //Address of the set flag in the EEPROM
 int x = matrix.width();
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,6 +46,18 @@ void setup() {
 	matrix.setTextWrap(false);
 	matrix.setBrightness(10);
 	matrix.setTextColor(GREEN);
+
+	rtc.begin();
+
+	if(EEPROM.read(setAddress) != 1)
+	{ 
+		rtc.setDOW(THURSDAY);     // Set Day-of-Week to Thursday
+		rtc.setTime(21, 00, 0);     // Set the time to 21:00:00 (24hr format)
+		rtc.setDate(22, 2, 2018);   //Set date to 22nd of Feb, 2018
+		EEPROM.write(setAddress, 1);
+	}
+
+	
 	
 	
 	Serial.begin(9600);
@@ -49,16 +70,27 @@ void loop() {
 	{
 	case 0:
 		Serial.println("in mode 0 - 24hr clock");
+		
 		matrix.fillScreen(0);
 		matrix.setCursor(x, 0);
-		matrix.print(F("Howdy"));
-		if (--x < -36) {
+		matrix.print(rtc.getTimeStr());
+		if (--x < -50) {
 			x = matrix.width();
 		}
 		matrix.show();
 		delay(100);
 		break;
 	case 1:
+		Serial.println("in mode 1 - date");
+		matrix.fillScreen(0);
+		matrix.setCursor(x, 0);
+		matrix.print(rtc.getDateStr());
+		if (--x < -50) {
+			x = matrix.width();
+		}
+		matrix.show();
+		delay(100);
+
 		break;
 	case 2:
 		break;
